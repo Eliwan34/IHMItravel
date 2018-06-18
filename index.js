cb28ac540dcc0a92afef8a1a2ccd94804a678291
Eliwan34 <38690804+Eliwan34@users.noreply.github.com>
2018-06-19 00:16:02 +0200

    19/06/2018

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongo = require('mongodb');
+
+let MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("students");
+  dbo.collection("websitecreationclass").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
