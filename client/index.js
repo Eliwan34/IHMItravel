f260ccbd0512968a83e1868c8272bd4d621df8bd
Eliwan34 <38690804+Eliwan34@users.noreply.github.com>
2018-06-20 13:23:05 +0200

    20/06/2018

---
+++ client/index.js
@@ -0,0 +1,26 @@
+function Pyrenees() {
+  var x = document.getElementById("pyrenees");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function Britany() {
+  var x = document.getElementById("britany");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function Lozere() {
+  var x = document.getElementById("lozere");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
---
+++ web.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb');
+
+
+    app.listen(2828, () => console.log('Example app listening on port 3000!'));
+});
